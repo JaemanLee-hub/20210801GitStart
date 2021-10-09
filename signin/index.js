3c1b686fb060096a03eb6819432040fa64ed837e
JaemanLee-hub <gand123@naver.com>
2021-10-09 20:59:38 +0900

    통신예제

--- signin/index.js
+++ signin/index.js
@@ -8,10 +8,29 @@ document.getElementById('btn_submit').addEventListener('click', () => {
   //fetch 브라우저 기본 ajax같은거 , HTTP 요청시 , 비동기
   // 리턴값이 promise 면 then함수를 쓸수있다
   fetch('http://localhost:3000/user').then((response) => {
-    console.log(response)
+    //console.log(response)
     //response.json 받아온 데이터를 사용하기위해 실행하는 함수
     response.json().then((data) => {
-      console.log(data)
+      //console.log(data)
+      const mail = document.getElementById('email').value
+      const pwd = document.getElementById('password').value
+      const createNameP = document.createElement('p')
+      const createEmailP = document.createElement('p')
+      if (!mail || !pwd) {
+        alert('로그인실패')
+        return false
+      }
+
+      if (mail === data.email && pwd === data.password) {
+        alert('로그인성공')
+        createNameP.textContent = data.name
+        createEmailP.textContent = data.email
+        document.getElementById('login').appendChild(createNameP)
+        document.getElementById('login').appendChild(createEmailP)
+      } else {
+        alert('로그인실패')
+        return false
+      }
     })
   })
 })
